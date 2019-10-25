341df189fe10faed9428a4073508b89a003b4b50
Fanch <francois.gerbes@sb-roscoff.fr>
2019-10-25 14:08:44 +0200

    add a python version check in test

--- tests.sh
+++ tests.sh
@@ -3,6 +3,7 @@
 set -e
 
 OS_LIST="ubuntu debian centos fedora"
+#OS_LIST="fedora"
 
 for OS_NAME in $OS_LIST
 do
@@ -14,8 +15,11 @@ do
     docker run --privileged --name ${OS_NAME}-with-systemd -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${OS_NAME}-with-systemd
 done
 
+sleep 30
+
 for OS_NAME in $OS_LIST
 do
+    docker exec -it ${OS_NAME}-with-systemd python --version
     docker exec -it ${OS_NAME}-with-systemd systemctl status
 done
 
